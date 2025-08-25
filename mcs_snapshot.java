dede79f2cf4b9c85463f4f12e775e0aef57e7244
jreleaserbot <jreleaser@kordamp.org>
2025-08-25 07:09:44 +0000

    mcs-jar early-access

--- mcs_snapshot.java
+++ mcs_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 24+
 //REPOS jitpack
 //DEPS com.github.mthmulders:mcs-jar:main-SNAPSHOT
-// Generated with JReleaser 1.19.0 at 2025-08-25T06:59:47.625058655Z
+// Generated with JReleaser 1.19.0 at 2025-08-25T07:09:34.882023033Z
 
 public class mcs_snapshot {
     public static void main(String... args) throws Exception {
