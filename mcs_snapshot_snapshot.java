dd0276f23ee471a2c8455a7c60ee3855a2230c93
jreleaserbot <jreleaser@kordamp.org>
2025-11-17 19:43:30 +0000

    mcs-snapshot-jar early-access

---
+++ mcs_snapshot_snapshot.java
@@ -0,0 +1,11 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25+
+//REPOS jitpack
+//DEPS it.mulders:mcs:main-SNAPSHOT
+// Generated with JReleaser 1.20.0 at 2025-11-17T19:43:18.766351613Z
+
+public class mcs_snapshot_snapshot {
+    public static void main(String... args) throws Exception {
+        it.mulders.mcs.App.main(args);
+    }
+}
