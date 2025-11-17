b342761d94198128b4c203e330be09e5e5a22d86
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-11-17 21:54:21 +0100

    Restore snapshot of MCS through JBang

---
+++ mcs_snapshot.java
@@ -0,0 +1,11 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25+
+//REPOS jitpack
+//DEPS it.mulders:mcs:main-SNAPSHOT
+// Generated manually
+
+public class mcs_snapshot {
+    public static void main(String... args) throws Exception {
+        it.mulders.mcs.App.main(args);
+    }
+}
