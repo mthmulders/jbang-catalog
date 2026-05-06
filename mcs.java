0a1cbd85dc6014ee14b4bfe5eb4efeb72a43043f
jreleaserbot <jreleaser@kordamp.org>
2026-05-06 14:00:23 +0100

    mcs-jar v0.10.0

--- mcs.java
+++ mcs.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 25+
-//DEPS it.mulders:mcs:0.9.7
-// Generated with JReleaser 1.23.0 at 2026-03-16T17:47:32.27127+01:00
+//DEPS it.mulders:mcs:0.10.0
+// Generated with JReleaser 1.24.0
 
 public class mcs {
     public static void main(String... args) throws Exception {
